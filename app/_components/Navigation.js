474580ac11fa255687be8217a51ecf39cac5eabb
is3388 <meiychin@yahoo.com>
2024-10-08 17:35:48 -0400

    Integrated authentication into the app by using auth.js lib

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,14 +1,60 @@
-import Link from "next/link"
+import Link from 'next/link';
+import { auth } from '../_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+  // read cookies from incoming request (runtime not build time - dynamic route)
+  // this component is within the header is for every route, so each route turns into dynamic and the entire website becomes dynamic
+  // call the auth function only in specific components or pages where dynamic behavior is necessary if you don't want the entire site dynamic
+  const session = await auth();
+  console.log(session); //return user object with name, email and image from gmail a/c
   return (
     <nav className='z-10 text-xl'>
-    <ul className='flex gap-16 items-center'>
-      <li><Link href='/' className='hover:text-accent-400 transition-colors'>Home</Link></li>
-      <li><Link href='/cabins' className='hover:text-accent-400 transition-colors'>Cabins</Link></li>
-      <li><Link href='/about' className='hover:text-accent-400 transition-colors'>About</Link></li>
-      <li><Link href='/account' className='hover:text-accent-400 transition-colors'>Guest account</Link></li>
-    </ul>
+      <ul className='flex gap-16 items-center'>
+        <li>
+          <Link href='/' className='hover:text-accent-400 transition-colors'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='/cabins'
+            className='hover:text-accent-400 transition-colors'
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='/about'
+            className='hover:text-accent-400 transition-colors'
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          {session?.user?.image ? (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors flex items-center gap-4'
+            >              
+                <img
+                  className='h-8 rounded-full'
+                  src={session.user.image}
+                  alt={session.user.name}
+                  referrerPolicy='no-referrer'
+                />              
+              <span>Guest account</span>
+            </Link>
+          ) : (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors'
+            >
+              Guest account
+            </Link>
+          )}
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,17 +1,27 @@
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
 import { getSettings, getBookedDatesByCabinId } from '../_lib/data-service';
+import { auth } from '../_lib/auth';
+import LoginMessage from './LoginMessage';
 
-export default async function Reservation({cabin}) {
+export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
-  
+
+  const session = await auth();
+
   return (
     <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
-          <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-          <ReservationForm cabin={cabin} />
-        </div>
-  )
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : <LoginMessage />}
+    </div>
+  );
 }
